e2e3caa8ba17ec1fe639c6214e0b780f2544e63d
Patrick <lupatrick13@gmail.com>
2020-10-26 18:56:39 -0700

    Fixed hilbert to make code 10000000 time more efficient

--- 152lab6/backend/compiler/StatementGenerator.cpp
+++ 152lab6/backend/compiler/StatementGenerator.cpp
@@ -22,33 +22,8 @@ struct CustomSort final
 			return LHS.first.first[1] < RHS.first.first[1];
 		else if(LHS.first.second == Predefined::realType)
 			return stof(LHS.first.first) < stof(RHS.first.first);
-		else if(LHS.first.second == Predefined::integerType)
+		else if(LHS.first.second == Predefined::integerType || LHS.first.second->getForm() == Form::ENUMERATION)
 			return stoi(LHS.first.first) < stoi(RHS.first.first);
-		else if(LHS.first.second->getForm() == Form::ENUMERATION)
-		{
-			vector<SymtabEntry *> *EnumConst1 = LHS.first.second->getEnumerationConstants();
-			vector<SymtabEntry *> *EnumConst2 = RHS.first.second->getEnumerationConstants();
-			int index1 = 0;
-			int index2 = 0;
-			string name1 = LHS.first.first;
-			string name2 = RHS.first.first;
-			transform(name1.begin(), name1.end(), name1.begin(), ::tolower);
-			transform(name2.begin(), name2.end(), name2.begin(), ::tolower);
-			for(SymtabEntry* Const1 : *EnumConst1)
-			{
-				if(Const1->getName() == name1)
-					break;
-				index1++;
-			}
-			for(SymtabEntry* Const2 : *EnumConst2)
-			{
-				if(Const2->getName() == name2)
-					break;
-				index2++;
-			}
-			return index1 < index2;
-
-		}
 		return true;
 	}
 };
@@ -142,7 +117,7 @@ void StatementGenerator::emitCase(PascalParser::CaseStatementContext *ctx)
 			for(PascalParser::CaseConstantContext *constant: Branch->caseConstantList()->caseConstant())
 			{
 				Typespec *type = constant->type;
-				string Constant = constant->getText();
+				string Constant = type->getForm() == Form::ENUMERATION ? to_string(constant->value) : constant->getText();
 				pair<string,Typespec* > ConstantPair = {Constant,type};
 				pair< pair<string,Typespec*>, int> ConstantandLabel = {ConstantPair, index};
 				ConstantList.insert(ConstantandLabel);
@@ -156,22 +131,8 @@ void StatementGenerator::emitCase(PascalParser::CaseStatementContext *ctx)
 			emitLabel(Constants.first.first[1], Labels[Constants.second]);
 		else if(Constants.first.second == Predefined::realType)
 			emitLabel(stof(Constants.first.first), Labels[Constants.second]);
-		else if(Constants.first.second == Predefined::integerType)
+		else if(Constants.first.second == Predefined::integerType || Constants.first.second->getForm() == Form::ENUMERATION)
 			emitLabel(stoi(Constants.first.first), Labels[Constants.second]);
-		else if(Constants.first.second->getForm() == Form::ENUMERATION)
-		{
-			vector<SymtabEntry *> *EnumConst = Constants.first.second->getEnumerationConstants();
-			int index = 0;
-			string name = Constants.first.first;
-			transform(name.begin(), name.end(), name.begin(), ::tolower);
-			for(SymtabEntry* Const : *EnumConst)
-			{
-				if(Const->getName() == name)
-					break;
-				index++;
-			}
-			emitLabel(index, Labels[Constants.second]);
-		}
 		else
 			emitLabel(Constants.first.first, Labels[Constants.second]);
 	}
